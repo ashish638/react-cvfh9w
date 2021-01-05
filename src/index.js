ba5cb61638469a94793e99d9672b3b5f4e8cdd1d
ashish638 <ashishashish.srivastava98@gmail.com>
2021-01-05 14:24:14 +0530

    12 files added, 4 deleted

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Avatar from "./Avatar";
-import './style.css';
-
-
-
-
-ReactDOM.render(<Avatar name= "vinod bahadur thapa"/>, document.getElementById('root'));

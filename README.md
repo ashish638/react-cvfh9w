2a025db31dc1b0b400b509d1d989311f2592cf68
ashish638 <ashishashish.srivastava98@gmail.com>
2021-01-05 13:58:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-cvfh9w
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cvfh9w)
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Avatar from "./Avatar";
+import './style.css';
+
+
+
+
+ReactDOM.render(<Avatar name= "vinod bahadur thapa"/>, document.getElementById('root'));
